5a51941f25848a27c114b9434b9bc32f1bd4d2a4
Enoch Poon <enoch.poon99@gmail.com>
2021-03-17 13:19:12 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const inputRef = React.useRef();
+  const [loading, setLoading] = useState(false);
+  const [results, setResults] = useState(null);
+  function onSearch() {
+    const val = inputRef.current.value;
+    if (val === '') return;
+    setLoading(true);
+    setResults(null);
+    fetch('https://www.metaweather.com/api/location/search/?query=' + val)
+    .then(res => res.json())
+    .then(data => {
+      if (data.length === 0) {
+        setResults('ERROR');
+        setLoading(false);
+      } else {
+        const woeid = data[0].woeid;
+        fetch(`https://www.metaweather.com/api/location/${woeid}/`)
+        .then(res => res.json())
+        .then(res => {
+          setResults(res);
+          setLoading(false);
+        })
+      }
+      
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      City <input ref={inputRef}/>
+      <button onClick={onSearch}>Search</button>
+      <div>{loading && 'loading'}</div>
+      <div>{results && (results !== 'ERROR'? (
+        <>
+        
+        <table>
+          <caption>{results.title}</caption>
+          <tbody>
+            <tr>
+              {results.consolidated_weather.map(fc => <th key={fc.applicable_date}>{fc.applicable_date}</th>)}
+            </tr>
+            <tr>
+              {results.consolidated_weather.map(fc => (
+                <td key={fc.applicable_date}>
+                  <div>{fc.weather_state_name}</div>
+                  <div>{Math.round(fc.the_temp)}°C</div>
+                </td>
+              ))}
+            </tr>
+          </tbody>
+          
+        </table>
+        </>
+      ) : 'ERROR')}</div>
     </div>
   );
 }
